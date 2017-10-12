63f1ea69187ff9a4265bd99075b046cb086fc356
kevincwu0 <kevincwu0@gmail.com>
2017-10-12 14:23:50 +0100

    Add dybrid model and CSV file

---
+++ hybrid_model.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Part 1 - Identify the Frauds with the Self-Organizing Map
+
+# Part 2 - Going from Unsupervised to Supervised Deep Learning
+
+# Combine the two models, take input from results of SOMs
+# Ranking of predicted probablity of each customers cheated
